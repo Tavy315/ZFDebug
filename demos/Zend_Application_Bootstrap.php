610de459e0d348c2e973c9c02a6f65880dd4c469
Octav <tavy315@users.noreply.github.com>
2016-11-10 16:26:46 +0200

    Update Zend_Application_Bootstrap.php

--- demos/Zend_Application_Bootstrap.php
+++ demos/Zend_Application_Bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+class Bootstrap extends \Zend_Application_Bootstrap_Bootstrap
 {
     protected function _initZFDebug()
     {
         // Setup autoloader with namespace
-        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader = \Zend_Loader_Autoloader::getInstance();
         $autoloader->registerNamespace('ZFDebug');
 
         // Ensure the front controller is initialized
@@ -17,7 +17,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         // Only enable zfdebug if options have been specified for it
         if ($this->hasOption('zfdebug')) {
             // Create ZFDebug instance
-            $zfDebug = new ZFDebug_Controller_Plugin_Debug($this->getOption('zfdebug'));
+            $zfDebug = new \ZFDebug\Controller\Plugin\Debug($this->getOption('zfdebug'));
 
             // Register ZFDebug with the front controller
             $front->registerPlugin($zfDebug);
@@ -32,7 +32,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
         // Plugins that take objects as parameters like Database and Cache
         // need to be registered manually:
-        $zfDebug->registerPlugin(new ZFDebug_Controller_Plugin_Debug_Plugin_Database($db));
+        $zfDebug->registerPlugin(new \ZFDebug\Controller\Plugin\Debug\Plugin\Database($db));
 
         // Alternative configuration without application.ini
         $options = [
@@ -48,7 +48,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
                 'exception',
             ],
         ];
-        $zfDebug = new ZFDebug_Controller_Plugin_Debug($options);
+        $zfDebug = new \ZFDebug\Controller\Plugin\Debug($options);
         // Register ZFDebug with the front controller
         $front->registerPlugin($zfDebug);
     }
